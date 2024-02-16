40f8d067cae6ee8aff9766b3e256c0f47a1a9a65
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2024-02-16 00:34:15 -0300

    fonts v2

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -108,7 +108,7 @@ const Login = () => {
            
                 <div className="mt-6 text-center ">
                     <Link to='/register' className="text-3xl text-blue-500 hover:underline dark:text-blue-400">
-                    ¿No tienes cuenta? Registrate
+                    ¿No tienes cuenta? <span className="underline font-semibold italic">Registrate</span> 
                     </Link>
                 </div>
             </div>
